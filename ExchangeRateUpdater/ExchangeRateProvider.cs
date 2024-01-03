e3429e8afb561e8e780efdc2dfcc1a675f39b04b
Steve Pettifer <stephen.pettifer@guestline.com>
2024-01-03 11:58:05 +0000

    Adds skeleton and test project

---
+++ ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRateProvider
+    {
+        /// <summary>
+        /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
+        /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
+        /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
+        /// some of the currencies, ignore them.
+        /// </summary>
+        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+        {
+            // https://api.cnb.cz/cnbapi/swagger-ui.html#/
+            return Enumerable.Empty<ExchangeRate>();
+        }
+    }
+}
